34eab93a7999a48ec38d4358507b31c22ffcfb1c
gugucazhiz <gustavo_msn_@hotmail.com>
2025-03-21 08:57:04 -0300

    primeiro-push

---
+++ 1hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string abc;
+    cout << "Entrada 2 values";
+    cin >> abc;
+    cout << "Hello World";
+    cout << "\n"+abc+"\n";
+    return 0;
+}
