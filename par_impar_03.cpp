f71763143a31374dbd65312bcb0d761b71952776
gugucazhiz <domateklh@gmail.com>
2025-08-19 08:40:22 -0300

    atualizando

---
+++ par_impar_03.cpp
@@ -0,0 +1,28 @@
+﻿ #include <iostream>
+ using namespace std;
+
+
+ int main(){
+
+  int vetorEx[15];
+  string resposta;
+
+  cout << "P ou I" << endl;
+  cin >> resposta;
+
+  if (resposta == "P") {
+      for(int i=2;i <=15;i+=2){
+        vetorEx[i] = i;
+        cout << vetorEx[i] <<", ";
+    }
+  }
+  else{
+    for(int i=1;i <=15;i+=2){
+        vetorEx[i] = i;
+        cout << vetorEx[i] << ", ";
+    }
+  }
+
+
+    return 0;
+ }
